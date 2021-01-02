b67a3511c3d8021cb6aaba41a775ea6fdfc936a5
ZiniZhu <zinizhu0825@gmail.com>
2021-01-02 12:18:37 -0800

    add d3 and image examples

--- barchart.js
+++ barchart.js
@@ -1,5 +1,5 @@
 // define margin and svg size
-var margin = { top: 50, bottom: 50, left: 50, right: 50 }
+var margin = { top: 50, bottom: 50, left: 50, right: 150 }
 var width = 800
 var height = 600
 
@@ -12,6 +12,43 @@ var exampleBarChartSvg = d3
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-  d3.csv('./files/data/lebron.csv', data => {
-    console.log(data) // check the input data format in the browser console
+d3.csv('./files/data/lebron.csv', data => {
+  console.log(data) // check the input data format in the browser console
+
+  var stats = []
+  var seasons = []
+  data.forEach(row => {
+    stats.push({ season: row['Season'], PTS: +row['PTS'] })
+    seasons.push(row['Season'])
   })
+  console.log(stats)
+  console.log(seasons)
+
+  // step 1: create scale
+  // for x-axis (seasons), use scaleLinear
+  var xScale = d3
+    .scaleLinear()
+    .domain([-0.5, seasons.length-0.5])
+    .range([0, width])
+
+  exampleBarChartSvg
+    .append('g')
+    .attr('transform', 'translate(' + 0 + ', ' + height + ')')
+    .call(
+      d3
+        .axisBottom(xScale)
+        .tickSize(2)
+        .ticks(seasons.length)
+        .tickFormat(d => seasons[d])
+    )
+
+    // for y-axis (seasons), use scaleLinear
+
+    // step 2: create bars (by appending rectangles)
+
+    // step 3: add x-axis, y-axis, chart title
+
+    // step 4: adjust styles, e.g. font size
+
+  
+})
